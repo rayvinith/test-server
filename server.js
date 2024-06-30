92fa76e696751c6f981e841c54a73aaa2a93b4d3
VINITH RAY <112755835+Its-me-vinith@users.noreply.github.com>
2024-06-30 11:41:52 -0700

    addidng static files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const path=require("path");
 //env config
 dotenv.config();
 
@@ -26,6 +26,15 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/blog", blogRoutes);
+
+
+
+//static files 
+app.use(express.static(path.join(__dirname,'./client')))
+app.get('*', function(req,res){
+res.sendFile(path.join(__dirname,'./client/index.html'))
+})
+
 
 // Port
 const PORT = process.env.PORT || 10000;
